26408fc06bb80c805151af2299ab2692b4eaa27c
emersoncrp <emersoncrp@gmail.com>
2025-05-23 11:26:20 -0500

    fin maquina 145

--- apps/produccion/serializers.py
+++ apps/produccion/serializers.py
@@ -2,6 +2,8 @@
 
 from rest_framework import serializers
 
+from apps.login.serializers import UserSerializer
+
 from .models import Producto, Maquina, Turno, RegistroR145
 
 class ProductoSerializer(serializers.ModelSerializer):
@@ -32,6 +34,11 @@ class RegistroR145Serializer(serializers.ModelSerializer):
         slug_field='code',
         queryset=Turno.objects.all()
     )
+
+    operario = UserSerializer(read_only=True)
+    revisador = UserSerializer(read_only=True)
+    auxiliar = UserSerializer(read_only=True)
+
     hora_inicio = serializers.TimeField(format="%H:%M", input_formats=["%H:%M"])
     hora_fin = serializers.TimeField(format="%H:%M", input_formats=["%H:%M"])
     extension_hora = serializers.TimeField(format="%H:%M", input_formats=["%H:%M"])
