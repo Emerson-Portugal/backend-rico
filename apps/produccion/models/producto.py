e390ba9c18c3f915cf95f51bc007b89d74c2915b
emersoncrp <emersoncrp@gmail.com>
2025-06-15 16:46:12 -0500

    update producto

--- apps/produccion/models/producto.py
+++ apps/produccion/models/producto.py
@@ -4,11 +4,12 @@ from django.db import models
 class Producto(models.Model):
     code  = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=100)
+    day = models.IntegerField(default=0, null=True)
     type = models.CharField(max_length=20, choices=[
         ('CRUDO', 'CRUDO'),
         ('COCIDO', 'COCIDO')
     ])
 
     def __str__(self):
-        return f"{self.code} - {self.name}"
+        return f"{self.code} - {self.name} - {self.day}"
 
