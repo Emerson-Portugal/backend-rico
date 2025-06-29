4057f0d137ba9369e84c5d99e993b05c03bc32ec
emersoncrp <emersoncrp@gmail.com>
2025-06-29 14:36:30 -0500

    new db

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,13 @@
 #core/settings.py
 from pathlib import Path
 from corsheaders.defaults import default_headers
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Cargar archivo .env
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,14 +107,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_rico',
-        'USER': 'db_rico_user',
-        'PASSWORD': 'dxiIk72Cw8WUP0RrSF8dQnYulh1t6PWb',
-        'HOST': 'dpg-d0roor6mcj7s73d5nkog-a.oregon-postgres.render.com',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
